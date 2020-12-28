8d885d4f92e2a23de43f02ec170ba36023d67d50
Piotr Biesiada <39916932+pbies@users.noreply.github.com>
2020-12-28 02:04:28 +0100

    Update ytdl.bat

--- ytdl.bat
+++ ytdl.bat
@@ -1,2 +1,5 @@
-@youtube-dl -i -f bestvideo[ext=mp4]+bestaudio[ext=m4a]/bestvideo+bestaudio --merge-output-format mp4 "%1"
-@pause
+@echo off
+chcp 65001
+youtube-dl -i -f bestvideo[ext=mp4]+bestaudio[ext=m4a]/bestvideo+bestaudio --merge-output-format mp4 "%1" -o "%%(upload_date)s %%(title)s.%%(ext)s"
+echo 
+pause
