6b29bf8a5efa20a31199644d575a99d209c32b71
Piotr Biesiada <39916932+pbies@users.noreply.github.com>
2021-06-21 01:42:01 +0200

    fix

--- defrag-all-disks.bat
+++
@@ -1,4 +0,0 @@
-for %%a in (c d e f g h i j k l m n o p q r s t u v w x y z) do defrag /h /u /v %%a:
-for %%a in (c d e f g h i j k l m n o p q r s t u v w x y z) do defrag /h /u /v %%a:
-for %%a in (c d e f g h i j k l m n o p q r s t u v w x y z) do defrag /h /u /v %%a:
-pause
--- fix-win-update.bat
+++ fix-win-update.bat
@@ -5,4 +5,5 @@ del /f /s /q %windir%\SoftwareDistribution
 del /f /s /q %windir%\System32\CatRoot2
 net start wuauserv
 net start cryptsvc
+echo 
 pause
